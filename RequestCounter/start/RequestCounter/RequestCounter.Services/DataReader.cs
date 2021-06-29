b59282fe92ac5b7ef232e9449c698692c88d2c0f
Konrad Korf <konrad.korf@gmail.com>
2021-06-29 21:48:29 +0200

    Final

--- RequestCounter/start/RequestCounter/RequestCounter.Services/DataReader.cs
+++ RequestCounter/start/RequestCounter/RequestCounter.Services/DataReader.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace RequestCounter.Services
@@ -14,14 +15,41 @@ namespace RequestCounter.Services
 
         internal static Dictionary<string, int> ReadFromFile()
         {
-            throw new NotImplementedException();
-           //Put code reading data from file here
+            var linesOfFile = File.ReadAllLines(GetFullFilePath());
+
+            // var stats = new Dictionary<string, int>();
+            // foreach (var line in linesOfFile)
+            // {
+            //     var splitted = line.Split(",");
+            //     if (splitted.Length == 2)
+            //     {
+            //         stats.Add(splitted[0], int.Parse(splitted[1]));
+            //     }
+            // }
+            //
+            // return stats;
+            
+            return linesOfFile
+                .Select(line => line.Split(","))
+                .Where(splitted => splitted.Length == 2)
+                .ToDictionary(splitted => splitted[0], splitted => int.Parse(splitted[1]));
         }
 
         internal static void WriteToFile(Dictionary<string, int> stats)
         {
-            throw new NotImplementedException();
-            //Put code writing to file here
+            // List<string> fileEntries = new List<string>();
+            // foreach (var entry in stats)
+            // {
+            //     fileEntries.Add($"{entry.Key},{entry.Value}");
+            // }
+            //
+            // File.WriteAllLines(GetFullFilePath(), fileEntries);
+            
+            var fileEntries = stats.Select(entry => $"{entry.Key},{entry.Value}").ToList();
+
+            File.WriteAllLines(GetFullFilePath(), fileEntries);
+            
+            
         }
 
         /// <summary>
--- RequestCounter/start/RequestCounter/RequestCounter.Services/RequestCountStatisticsService.cs
+++ RequestCounter/start/RequestCounter/RequestCounter.Services/RequestCountStatisticsService.cs
@@ -10,10 +10,11 @@ namespace RequestCounter.Services
     /// Use this class to implement IRequestCountStatsService
     /// </summary>
     public class RequestCountStatisticsService : IRequestCountStatsService
-    {     
+    {
         //List of methods which are allowed to be counted by designed service
-        private static readonly string[] AllowedMethods = new string[] { "GET", "POST", "DELETE", "PATCH", "PUT" };
-        private static Dictionary<string, int> _requestCount = new Dictionary<string, int>();
+        private static readonly string[] AllowedMethods = new string[] {"GET", "POST", "DELETE", "PATCH", "PUT"};
+        private static Dictionary<string, int> _requestCount;
+        private readonly object objectLock = new object();
 
         // public Dictionary<string, int> RequestCount
         // {
@@ -27,11 +28,18 @@ namespace RequestCounter.Services
             {
                 return;
             }
-            
-            _requestCount.TryGetValue(method, out var currentCount); 
-            _requestCount[method] = currentCount + 1;
-            
-            
+
+            lock (objectLock)
+            {
+                _requestCount = DataReader.ReadFromFile();
+
+                _requestCount.TryGetValue(method, out var currentCount);
+                _requestCount[method] = currentCount + 1;
+
+                DataReader.WriteToFile(_requestCount);
+            }
+
+
             // if (_requestCount.Keys.Contains(method))
             // {
             //     _requestCount[method] += 1;
@@ -44,7 +52,14 @@ namespace RequestCounter.Services
 
         public Stats GetStatistics()
         {
-            return new Stats(){ Counts = _requestCount};
+            {
+                lock (objectLock)
+                {
+                    _requestCount = DataReader.ReadFromFile();
+                }
+
+                return new Stats() {Counts = _requestCount};
+            }
         }
     }
 }
