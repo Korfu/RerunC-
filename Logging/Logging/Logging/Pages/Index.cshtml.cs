76b13526f98b9d24c7d9ec12a9c197fda12dffc5
Konrad Korf <konrad.korf@gmail.com>
2021-06-30 22:43:00 +0200

    getting data from config

--- Logging/Logging/Logging/Pages/Index.cshtml.cs
+++ Logging/Logging/Logging/Pages/Index.cshtml.cs
@@ -5,19 +5,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace Logging.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _configuration;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public string Message { get; set; }
+
+        public string PageTitle => _configuration["PageTitle"];
 
         public void OnGet()
         {
